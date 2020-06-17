b03b1a6a1e67a8606c9b68aad9c5fca1ab86ee0a
Emiliano Dalla Verde Marcozzi <edvm@fedoraproject.org>
2020-06-16 21:42:00 -0300

    udp client to test

--- foo.py
+++ foo.py
@@ -3,24 +3,22 @@ import aioredis
 
 
 async def reader(ch):
-    while (await ch.wait_message()):
+    while await ch.wait_message():
         msg = await ch.get_json()
         print("Got Message:", msg)
 
 
 async def main():
-    sub = await aioredis.create_redis(
-        'redis://localhost/1')
-    res = await sub.subscribe('sensors')
+    sub = await aioredis.create_redis("redis://localhost/1")
+    res = await sub.subscribe("sensors")
     ch1 = res[0]
 
     tsk = await reader(ch1)
 
-
-    await sub.unsubscribe('sensors')
+    await sub.unsubscribe("sensors")
     await tsk
     sub.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
